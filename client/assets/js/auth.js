cb4a9b58c725c0efcf1b5382b2e8febae2965d44
Gitesh Sharma <gitesh15796@gmail.com>
2025-07-24 22:03:23 +0530

    navbar modified

--- client/assets/js/auth.js
+++ client/assets/js/auth.js
@@ -1,6 +1,7 @@
 (function () {
     const user = JSON.parse(localStorage.getItem('user'));
     console.log(user)
+     document.getElementById('user-name').textContent = user.name;
      document.getElementById('user-email').textContent = user.email;
     if (!user) {
         window.location.href = 'login.html';
